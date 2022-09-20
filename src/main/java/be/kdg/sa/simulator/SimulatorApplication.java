e632213593f5e7b8731a63ec3807092ef6f119e2
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2022-09-20 19:26:46 +0200

    Created project

---
+++ src/main/java/be/kdg/sa/simulator/SimulatorApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.sa.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorApplication {
+	
+	public static void main (String[] args) {
+		SpringApplication.run (SimulatorApplication.class, args);
+	}
+	
+}
