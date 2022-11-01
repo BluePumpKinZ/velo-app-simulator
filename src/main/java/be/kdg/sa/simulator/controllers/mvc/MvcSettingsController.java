7c8cc2acf2ad0f828b36b2b5a929abc7dbf5b971
Maxim Derboven <maxim.derboven@student.kdg.be>
2022-11-01 23:08:03 +0100

    Added adjusting of simulation settings from webpage

--- src/main/java/be/kdg/sa/simulator/controllers/mvc/MvcSettingsController.java
+++ src/main/java/be/kdg/sa/simulator/controllers/mvc/MvcSettingsController.java
@@ -1,9 +1,13 @@
 package be.kdg.sa.simulator.controllers.mvc;
 
 import be.kdg.sa.simulator.services.SettingService;
+import be.kdg.sa.simulator.settings.SettingValues;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,6 +24,14 @@ public class MvcSettingsController {
 	public String settings (Model model) {
 		model.addAttribute ("simSettings", settingService.getSettingValues ());
 		return "settings";
+	}
+	
+	@PostMapping (path = "/update")
+	public String updateSettings (@ModelAttribute("simSettings") SettingValues simSettings, BindingResult result, Model model) {
+		if (result.hasErrors ())
+			return "settings";
+		settingService.setSettingValues (simSettings);
+		return "redirect:/settings";
 	}
 
 }
